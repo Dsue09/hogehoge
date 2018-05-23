a7bbb58a34c422fe758e1a14f6f20270d019cf60
d.sue <d.sue.0909@gmail.com>
2018-05-23 11:47:27 +0900

    Initial Commit

---
+++ hogehoge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hogehoge
+//
+//  Created by 末吉大介 on 2018/05/23.
+//  Copyright © 2018年 D.sue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
